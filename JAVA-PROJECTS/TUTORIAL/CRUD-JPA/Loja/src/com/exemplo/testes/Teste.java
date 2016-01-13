c654cc19b991ec451d34a53cfcca333d636a802e
jimbrunop <jimbrunop@gmail.com>
2016-01-13 14:18:21 -0200

    tutorial JPA - CRUD básico

---
+++ JAVA-PROJECTS/TUTORIAL/CRUD-JPA/Loja/src/com/exemplo/testes/Teste.java
@@ -0,0 +1,39 @@
+package com.exemplo.testes;
+
+import java.util.List;
+
+import com.exemplo.entidade.Cliente;
+import com.exemplo.repositorio.RepositorioCliente;
+
+public class Teste {
+
+	public static void main(String args[]){
+		
+		
+
+		RepositorioCliente repositorioCliente = new RepositorioCliente();
+
+		Cliente cliente = new Cliente();
+//		teste inserir
+//		cliente.setNome("Fulano De tal");
+//		cliente.setIdade(30);
+//		repositorioCliente.salvar(cliente);
+		
+		
+		//teste listar todos
+//		List<Cliente> clientes = repositorioCliente.listarTodos();
+//		
+//		for (Cliente cliente : clientes) {
+//			System.out.println(cliente.getNome());
+//			
+//		}
+		
+// 		Remover
+// 		repositorioCliente.remover(cliente);
+		
+		
+	
+	
+	}
+	
+}
