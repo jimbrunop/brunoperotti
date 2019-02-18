7f3c3ffc519088c67a908795c8abe8ad77cf2aa7
Bruno <jimbrunop@gmail.com>
2019-02-18 19:27:46 -0300

    teste

---
+++ Python - Projects/Cricação de módulos/estudo.py
@@ -0,0 +1,21 @@
+#coding: utf-8
+
+
+
+a = 10
+
+b = "Módulo personalizado"
+
+print("Módulo executado")
+
+
+def f():
+    print("f1")
+    print("f2")
+    print("f3")
+    print("f4")
+    print("f5")
+
+f()
+
+print(__name__)
---
+++ Python - Projects/Cricação de módulos/main.py
@@ -0,0 +1,9 @@
+#coding: utf-8
+
+
+import estudo
+
+
+print("Programa acabou")
+
+print(__name__)
